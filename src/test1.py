69f1a5163f59f3c4ffd95f38477410c04a260ac4
Manavalan Gajapathy <manag@uab.edu>
2023-08-15 10:31:53 -0500

    adds python scripts for testing

---
+++ src/test1.py
@@ -0,0 +1,32 @@
+# Import pickle module
+import pickle
+
+# Declare the object to store data
+dataObject = []
+# Iterate the for loop for 5 times
+for num in range(10, 15):
+    dataObject.append(num)
+
+# Open a file for writing data
+file_handler = open("languages", "wb")
+
+# Dump the data of the object into the file
+pickle.dump(dataObject, file_handler)
+
+# close the file handler
+file_handler.close()
+
+# Open a file for reading the file
+file_handler = open("languages", "rb")
+
+# Load the data from the file after deserialization
+dataObject = pickle.load(file_handler)
+
+# Iterate the loop to print the data
+for val in dataObject:
+    print("The data value : ", val)
+
+# close the file handler
+file_handler.close()
+
+print(dddd)
